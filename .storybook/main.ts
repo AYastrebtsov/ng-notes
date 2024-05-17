f61f7ba62bbe2fdac9272a73d3a704a642f9def3
Andrey <yastrebsov@icloud.com>
2024-05-17 19:35:14 +0300

    test

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../app/**/*.mdx", "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  staticDirs: ["../public/"],
+  staticDirs: [{ from: "../public/", to: "../ng-notes/mockImg/" }],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -21,12 +21,6 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
-  },
-  async viteFinal(config) {
-    // config.base = process.env.BASE_PATH || config.base;
-    config.base = "/ng-notes/";
-    // return the customized config
-    return config;
   },
 };
 export default config;
